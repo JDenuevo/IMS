568c65768b42695522d1f1a8d9a40613eaa3b184
Jhemar Denuevo <122671038+JDenuevo@users.noreply.github.com>
2023-10-19 15:53:26 +0800

    IMS Demo

---
+++ src/pages/admin/warehouse.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>IMS - Warehouse</title>
+
+  <link rel="icon" href="../../assets/images/logos/ims.png">
+
+  <!-- Main Template -->
+  <link rel="stylesheet" href="../../assets/css/styles.min.css">
+
+</head>
+
+<body>
+
+<?php include '../admin/components/navigation.php'; ?>
+
+  <!--  Main wrapper -->
+  <div class="body-wrapper">
+
+    <?php include '../admin/components/header.php'; ?>
+
+      <div class="container-fluid">
+        <div class="card w-100">
+          <div class="card-body p-4">
+            <div class="d-flex">
+              <h5 class="card-title fw-semibold mb-4">Warehouses</h5>
+              <div class="flex-grow-1"></div>
+              <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                <i class="ti ti-square-plus"></i>
+                Add Warehouse
+              </button>
+            </div>
+            <div class="table-responsive">
+              <table class="table text-nowrap mb-0 align-middle">
+                <thead class="text-dark fs-4">
+                  <tr>
+                    <th class="border-bottom-0">
+                      <h6 class="fw-semibold mb-0">ID</h6>
+                    </th>
+                    <th class="border-bottom-0">
+                      <h6 class="fw-semibold mb-0">Assigned</h6>
+                    </th>
+                    <th class="border-bottom-0">
+                      <h6 class="fw-semibold mb-0">Warehouse Name</h6>
+                    </th>
+                    <th class="border-bottom-0">
+                      <h6 class="fw-semibold mb-0">Priority</h6>
+                    </th>
+                    <th class="border-bottom-0">
+                      <h6 class="fw-semibold mb-0">Budget</h6>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="border-bottom-0"><h6 class="fw-semibold mb-0">1</h6></td>
+                    <td class="border-bottom-0">
+                        <h6 class="fw-semibold mb-1">Jhemar Denuevo</h6>
+                        <span class="fw-normal">Web Designer</span>                          
+                    </td>
+                    <td class="border-bottom-0">
+                      <p class="mb-0 fw-normal">Jayniella Morente</p>
+                    </td>
+                    <td class="border-bottom-0">
+                      <div class="d-flex align-items-center gap-2">
+                        <span class="badge bg-primary rounded-3 fw-semibold">Low</span>
+                      </div>
+                    </td>
+                    <td class="border-bottom-0">
+                      <h6 class="fw-semibold mb-0 fs-4">$3.9</h6>
+                    </td>
+                  </tr>      
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+  </div>
+</div>
+
+<!-- Add Product -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Add a Warehouse</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="row g-2">
+          <div class="col-md">
+            <div class="form-floating">
+              <input type="email" class="form-control" id="floatingInputGrid" placeholder="name@example.com" value="mdo@example.com">
+              <label for="floatingInputGrid">Name</label>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="form-floating">
+              <select class="form-select" id="floatingSelectGrid">
+                <option selected>Category</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+              <label for="floatingSelectGrid">Works with selects</label>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary"><i class="ti ti-device-floppy"></i> Save</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<script src="../../assets/libs/jquery/dist/jquery.min.js"></script>
+<script src="../../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="../../assets/js/sidebarmenu.js"></script>
+<script src="../../assets/js/app.min.js"></script>
+<script src="../../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+<script src="../../assets/libs/simplebar/dist/simplebar.js"></script>
+<script src="../../assets/js/dashboard.js"></script>
+
+</body>
+</html>
---
+++ src/pages/user/components/navigation.php
@@ -0,0 +1,147 @@
+<!--  Body Wrapper -->
+  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+    data-sidebar-position="fixed" data-header-position="fixed">
+    <!-- Sidebar Start -->
+    <aside class="left-sidebar">
+      <!-- Sidebar scroll-->
+      <div>
+        <div class="brand-logo d-flex align-items-center justify-content-between">
+          <a href="./home.php" class="text-nowrap logo-img">
+            <div style="display: inline-block; vertical-align: middle;">
+              <img src="../../assets/images/logos/ims.png" alt="" class="img-fluid ms-4" style="max-height: 50px; max-width: 50px; vertical-align: middle;"/>
+            </div>
+            <div style="display: inline-block; vertical-align: middle;">
+              <label class="fs-6 ms-3 fw-bold">IMS'youu</label>
+            </div>
+          </a>
+          <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+            <i class="ti ti-x fs-8"></i>
+          </div>
+        </div>
+
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+          <ul id="sidebarnav">
+            <li class="nav-small-cap">
+              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+              <span class="hide-menu">GENERAL</span>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./home.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-home"></i>
+                </span>
+                <span class="hide-menu">Home</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./product.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-box-seam"></i>
+                </span>
+                <span class="hide-menu">Products</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./brand.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-brand-appgallery"></i>
+                </span>
+                <span class="hide-menu">Brands</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./category.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-category"></i>
+                </span>
+                <span class="hide-menu">Categories</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./warehouse.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-building-warehouse"></i>
+                </span>
+                <span class="hide-menu">Warehouse</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./module.php" aria-expanded="false">
+                <span>
+                    <i class="ti ti-chart-bar"></i>
+                </span>
+                <span class="hide-menu">Module</span>
+              </a>
+            </li>
+            <li class="nav-small-cap">
+              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+              <span class="hide-menu">REPORTS</span>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./sales.php" aria-expanded="false">
+                <span>
+                <i class="ti ti-currency-dollar"></i>
+                </span>
+                <span class="hide-menu">Sales</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./purchases.php" aria-expanded="false">
+                <span>
+                <i class="ti ti-shopping-cart"></i>
+                </span>
+                <span class="hide-menu">Purchases</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./stocks.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-packages"></i>
+                </span>
+                <span class="hide-menu">Stocks</span>
+              </a>
+            </li>
+            <li class="nav-small-cap">
+              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+              <span class="hide-menu">MANAGE</span>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./orders.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-truck-delivery"></i>
+                </span>
+                <span class="hide-menu">Orders</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./returns.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-truck-return"></i>
+                </span>
+                <span class="hide-menu">Returns</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./contacts.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-address-book"></i>
+                </span>
+                <span class="hide-menu">Contact</span>
+              </a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link" href="./accounts.php" aria-expanded="false">
+                <span>
+                  <i class="ti ti-user-circle"></i>
+                </span>
+                <span class="hide-menu">Account Management</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+      </div>
+      <!-- End Sidebar scroll-->
+    </aside>
+    <!--  Sidebar End -->
